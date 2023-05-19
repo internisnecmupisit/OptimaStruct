62e55a2f8018cbbf2864ea8646976e6d64d8b445
BestEngineer <bestpisit@hotmail.com>
2023-05-19 21:35:09 +0700

    Changes

--- web/app/index.js
+++ web/app/index.js
@@ -1,13 +1,9 @@
 const { default: axios } = require('axios');
 const express = require('express');
 const dotenv = require('dotenv');
-const ejs = require('ejs');
 dotenv.config();
 
 const app = express();
-
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
 
 // app.use(express.static('public'));
 
@@ -17,7 +13,5 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-    const apiUrl = `${process.env.API_URL}`;
-    res.render('index', { apiUrl });
-    // res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
