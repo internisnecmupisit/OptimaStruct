b3508d9aeda0f87c71c433b708955965b3479017
Pisit Best Pisuttipunpong <intern_isnecmu_pisit@manaosoftware.com>
2023-05-19 17:10:22 +0700

    Nicely Nice5

---
+++ config/powershell.ps1
@@ -0,0 +1,11 @@
+$loginserver = __loginserver__
+
+$username = __username__
+
+$password = __password__
+
+Write-Host "##vso[task.setvariable variable=username]$username"
+
+Write-Host "##vso[task.setvariable variable=password]$password"
+
+Write-Host "##vso[task.setvariable variable=loginserver]$loginserver"
