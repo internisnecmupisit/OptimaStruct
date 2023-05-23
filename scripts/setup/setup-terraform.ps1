44b2401b5083ac1f730aa0bbcdcd71b3fefc2d73
Pisit Best Pisuttipunpong <intern_isnecmu_pisit@manaosoftware.com>
2023-05-23 11:24:28 +0700

    update 07

--- scripts/setup/setup-terraform.ps1
+++ scripts/setup/setup-terraform.ps1
@@ -1,21 +1,21 @@
-if (az group show --name $(rgterraform)) {
-    if (az storage account show --name $(saterraform) --resource-group $(rgterraform)) {
+if (az group show --name $rgterraform) {
+    if (az storage account show --name $saterraform --resource-group $rgterraform) {
         exit 0
     }
     else {
-        az storage account create --name $(saterraform) --resource-group $(rgterraform) --location southeastasia --sku Standard_LRS
+        az storage account create --name $saterraform --resource-group $rgterraform --location southeastasia --sku Standard_LRS
 
-        az storage container create --name terraform --account-name $(saterraform)
+        az storage container create --name terraform --account-name $saterraform
 
-        az storage account keys list -g $(rgterraform) -n $(saterraform)
+        az storage account keys list -g $rgterraform -n $saterraform
     }
 }
 else {
-    az group create --name $(rgterraform) --location southeastasia
+    az group create --name $rgterraform --location southeastasia
 
-    az storage account create --name $(saterraform) --resource-group $(rgterraform) --location southeastasia --sku Standard_LRS
+    az storage account create --name $saterraform --resource-group $rgterraform --location southeastasia --sku Standard_LRS
 
-    az storage container create --name terraform --account-name $(saterraform)
+    az storage container create --name terraform --account-name $saterraform
 
-    az storage account keys list -g $(rgterraform) -n $(saterraform)
+    az storage account keys list -g $rgterraform -n $saterraform
 }
