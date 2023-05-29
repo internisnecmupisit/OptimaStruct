023e07e86272ecfec6e2cccd65ecb0c9bd895681
Pisit Best Pisuttipunpong <intern_isnecmu_pisit@manaosoftware.com>
2023-05-29 10:45:11 +0700

    Initial cv-prod

--- test.ps1
+++
@@ -1,7 +0,0 @@
-$webapps = az webapp list --resource-group rg-cv-prod --query "[?hostingEnvironmentProfile == null].{Name:name, DefaultHostName:defaultHostName}" --output tsv
-
-foreach ($webapp in $webapps -split "`n") {
-    $name, $defaultHostName = $webapp -split "`t"
-    $value = "https://$defaultHostName"
-    Write-Host "##vso[task.setvariable variable=$name]$value"
-}
